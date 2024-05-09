dbc59d8130bef088cc735a2d0a9e31e9717057b6
banafaDev <143547972+banafaDev@users.noreply.github.com>
2024-05-09 22:12:18 +0300

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 from pycaret.classification import *
+from pycaret.regression import *
 import io
 import seaborn as sns
 import pandas.api.types as pd_types
@@ -39,33 +40,78 @@ if file_upload is not None:
     else:
         st.error("Invalid file type. Please upload a CSV or Excel file.")
     
-    #diabetes = get_data('diabetes')
-    #predictions = predict_model(estimator=model,data=data)
+    
     
     
 
     
     st.divider()
+    numeric_columns = df.select_dtypes(include=["number"]).columns.tolist()
+    str_columns = df.select_dtypes(include=["object"]).columns.tolist()
 
+    st.subheader("Handle missing values for categorical columns")
+    if str_columns:
+        cat_impute_method = st.selectbox("Choose how to handle missing values in categorical columns",
+        ["Mode", "Class-based (Fill with 'Unknown')"],
+    )
 
-    df1 = df.dropna()
-    st.subheader("This your Data after Handling missing values")
-    st.dataframe(df1)
+        if cat_impute_method == "Mode":
+            for col in str_columns:
+                mode_value = df[col].mode()[0]  # Most frequent value
+                df[col].fillna(mode_value, inplace=True)
+            st.dataframe(df)    
+
+        elif cat_impute_method == "Class-based (Fill with 'Unknown')":
+            for col in str_columns:
+                df[col].fillna("Unknown", inplace=True)  # Fill with a default class
+            st.dataframe(df)        
+        
+    
+    st.divider()
+    
+
+    #st.dataframe(df)
+    st.subheader("Handle missing values for continuous columns")
+    if numeric_columns:
+        num_impute_method = st.selectbox(
+            "Choose how to handle missing values in continuous columns",
+            ["Mean", "Median", "Mode"],
+        )
+
+        if num_impute_method == "Mean":
+            for col in numeric_columns:
+                df[col].fillna(df[col].mean(), inplace=True)
+            st.dataframe(df)
+        elif num_impute_method == "Median":
+            for col in numeric_columns:
+                df[col].fillna(df[col].median(), inplace=True)
+            st.dataframe(df)
+        elif num_impute_method == "Mode":
+            for col in numeric_columns:
+                mode_value = df[col].mode()[0]
+                df[col].fillna(mode_value, inplace=True)
+            st.dataframe(df)
+    # Display the DataFrame after handling missing values
+    st.write("DataFrame after handling missing values:")
+    st.dataframe(df)
+
+    
+    
     st.divider()
 
 
     st.subheader("Some EDA on your data:")
     st.write("Summary statistics:")
-    st.dataframe(df1.describe())
+    st.dataframe(df.describe())
     
-    st.dataframe(df1.describe(include=['object','bool']))
+    st.dataframe(df.describe(include=['object','bool']))
 
     
 
 
-    numeric_columns = df1.select_dtypes(include=["number"]).columns.tolist()
-    str_columns = df1.select_dtypes(include=["object"]).columns.tolist()
-
+    
+    numeric_columns = df.select_dtypes(include=["number"]).columns.tolist()
+    str_columns = df.select_dtypes(include=["object"]).columns.tolist()
     
     st.write("Here we will do scatter graph for your data. So, please select x, y below")
     
@@ -87,68 +133,89 @@ if file_upload is not None:
 
     st.subheader("Now you can choose columns to delete them: ")
 
-    col = df1.columns.tolist()
+    col = df.columns.tolist()
 
-    select_del = st.multiselect("Select column to delete it :" , col)
-    df2 = df1.drop(columns= select_del)
-    st.dataframe(df2)
+    select_del = st.multiselect("Select column to delete it, if you want :" , col)
+    df = df.drop(columns= select_del)
+    st.dataframe(df)
 
     
     st.divider()
-    col_1 = df2.columns
-    numeric_columns_1 = df2.select_dtypes(include=["number"]).columns.tolist()
+    col_1 = df.columns
+    numeric_columns_1 = df.select_dtypes(include=["number"]).columns.tolist()
   
-    str_columns_1 = df2.select_dtypes(include=["object"]).columns.tolist()
+    str_columns_1 = df.select_dtypes(include=["object"]).columns.tolist()
 
 
 
-    st.subheader("Choose x,y to build the model")
-    st.write("*Note: you have to choose x,y from the same type.")
+    st.subheader("Choose target feature to build the model")
     
-    select_x = st.selectbox("choose x from your columns", col_1)
     
-    type_x = (df2[select_x].dtype)
-    if type_x in ("float64", "complex128","int64"):
-        select_y = st.selectbox("choose y from your columns", numeric_columns_1)
-        st.subheader("Regression")
-    elif type_x == "object":
-        select_y = st.selectbox("choose y from your columns", str_columns_1)
-        st.subheader("Classification")
+    
+    
+    
+    
+    
 
-        select_encode = st.radio("Please choose the type of encoding to catigorical columns:", ["Label Encoding", "One-Hot Encoding"])
+
+    st.subheader("Regression")
+    st.subheader("Classification")
+
+    select_encode = st.radio("Please choose the type of encoding to catigorical columns:",
+     ["Label Encoding", "One-Hot Encoding"])
+
+    if select_encode == "Label Encoding":
+        le = LabelEncoder()
+        for col in str_columns_1:
+            df[col] = le.fit_transform(df[col])
         
-        if select_encode == "Label Encoding":
-            le = LabelEncoder()
-            for col in str_columns_1:
-                df2[col] = le.fit_transform(df2[col])
+        st.dataframe(df)
+    
+    elif select_encode == "One-Hot Encoding":
+        df = pd.get_dummies(df, columns= str_columns_1)
+        st.dataframe(df)
             
-            st.dataframe(df2)
-        
-        elif select_encode == "One-Hot Encoding":
-            pd.get_dummies(df2, columns= str_columns_1)
-            st.dataframe(df2)
-            
-    else:
-        st.write("Please choose int , float or string column")
-
-        
     
+    
+    col_2 = df.columns
+    numeric_columns_1 = df.select_dtypes(include=["number"]).columns.tolist()    
+    str_columns_2 = df.select_dtypes(include=["object"]).columns.tolist()
+    
+    select_y = st.selectbox("choose y (target) from your columns", df.columns.tolist())
     st.divider()
+    st.dataframe(df)
     
     
-
-    target1 = st.selectbox("Please select your target feature:", col_1)
-
-    experiment = setup(df2,target= target1, categorical_features= str_columns_1)
-
-    best_model = compare_models()
     
-    predict_model(best_model, df2)
 
-    save_model(best_model, model_name='GR-model')
 
-    load_model('GR-model')
+    
+    type1= df[select_y].dtype
+    
+    if type1 == "object" :
+        experiment = setup(df,target= select_y, categorical_features= str_columns_2)
+        st.table(experiment.pull())
+        best_model = compare_models()
+        results = pull()
+        st.table(results)
+        predict_model(best_model, df)
+        st.text(best_model)
 
+    elif type1 in ("int64","float64"):
+        
+        s = setup(df, target = select_y, session_id = 123)
+        st.table(s.pull())
+        best = compare_models()
+        results1 = pull()
+        st.table(results1)
+        predict_model(best, df)
+        st.text(best)
+
+
+
+
+    
+    
 
     
     
@@ -157,16 +224,3 @@ if file_upload is not None:
     
 else: 
     st.write("Please upload CSV or Excel file.")
-
-#end ------------------------------------------------------------------------
-
-    """pycaret capstone 
-        1- data reading (Done)
-        1- apply eda (Done)
-        2- handling missing values (Done)
-        3- remove cols (Done)
-        4- choose x,y -> detect task type (classficatio or regression)(Done)
-        5- catgorical data encoding. choose the user >> 1,0 or label (Done)
-        6- pycaret 
-        in streamlit app"""
-
